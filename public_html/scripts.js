4608c2c909ddfe0d37393a40185c6ba08f1b75eb
Jourdan Wallace <jaywallace713@gmail.com>
2015-01-07 09:19:29 -0800

    I'm pretty sure I am now done with milestone 2, I am now on the third boring monotone filled video, and can't wait to continue.

---
+++ public_html/scripts.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+ $("document").ready(function() {
+     $(".p:first").css({"background-color": "purple", "color": "white"}); 
+     
+     $(".p:last").css({"background-color": "yellow", "color": "white"});
+     
+     $(".p span:first-child").css("background-color", "orange");
+});
